82a34c05c982e6954b731734448f55f2554e5f9e
Mike <maholzbach@gmail.com>
2017-02-21 18:20:45 +0100

    Added task addition

--- assets/JS/todos.js
+++ assets/JS/todos.js
@@ -1,10 +1,18 @@
-$("li").click(function(){
+$("ul").on("click", "li", function(){
   $(this).toggleClass("strikeout");
 });
 
-$(".trash").click(function(e){
+$("ul").on("click", ".trash", function(e){
   $(this).parent().fadeOut(function(){
     $(this).remove();
   });
   event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(e){
+  if(e.which === 13){
+    var todoText = $(this).val();
+    $(this).val("");
+    $("ul").append("<li><span class='trash'>X</span> " + todoText + "</li>");
+  }
 });
