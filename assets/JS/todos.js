55321d22edb0ab003d3fa2d57529aa8b6c0ffc69
Mike <maholzbach@gmail.com>
2017-02-22 18:28:32 +0100

    Completed

--- assets/JS/todos.js
+++ assets/JS/todos.js
@@ -13,6 +13,10 @@ $("input[type='text']").keypress(function(e){
   if(e.which === 13){
     var todoText = $(this).val();
     $(this).val("");
-    $("ul").append("<li><span class='trash'>X</span> " + todoText + "</li>");
+    $("ul").append("<li><span class='trash'><i class='fa fa-trash' aria-hidden='true'></i></span> " + todoText + "</li>");
   }
+});
+
+$(".fa-plus").click(function() {
+  $("input[type='text']").fadeToggle();
 });
