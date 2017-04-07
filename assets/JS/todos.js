f09d59603d16ca20c7d0b0558e190f449bc21529
Mike <maholzbach@gmail.com>
2017-04-07 19:59:06 +0200

    Added dynamic tracking table

--- assets/JS/todos.js
+++ assets/JS/todos.js
@@ -1,19 +1,33 @@
+var doneIndex = 0;
+var deleteIndex = 1;
+var addIndex = 1;
+
 $("ul").on("click", "li", function(){
+  if($(this).hasClass("strikeout")) {
+    doneIndex--;
+  } else {
+    doneIndex++;
+  }
   $(this).toggleClass("strikeout");
+  $("#done").html(doneIndex);
 });
 
 $("ul").on("click", ".trash", function(e){
   $(this).parent().fadeOut(function(){
     $(this).remove();
+    $("#deleted").html(deleteIndex);
+    deleteIndex++;
   });
   event.stopPropagation();
 });
 
 $("input[type='text']").keypress(function(e){
   if(e.which === 13){
+    $("#added").html(addIndex);
     var todoText = $(this).val();
     $(this).val("");
     $("ul").append("<li><span class='trash'><i class='fa fa-trash' aria-hidden='true'></i></span> " + todoText + "</li>");
+    addIndex++;
   }
 });
 
